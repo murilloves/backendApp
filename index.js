22548a1c656974fa29802fdd35232c52b6a736df
murilloves <murillo.vieiraesilva@gmail.com>
2019-01-19 14:17:02 -0200

    var port 8080

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ backendApp.get('/', (req, res) => {
     res.status(200).send('<h1>Hello Backend!!</h1>')
 })
 
-backendApp.listen(8080, () => {
-    console.log('Backend rodando...')
+port = 8080
+backendApp.listen(port, () => {
+    console.log(`Backend rodando na porta ${port}...`)
 })
