bcbe65668d843fb8821c85b8b37b032d2be51026
murilloves <murillo.vieiraesilva@gmail.com>
2019-02-20 23:33:50 -0300

    trying to work ci heroku git

--- server.js
+++ server.js
@@ -45,6 +45,4 @@ backendApp.use('/api/profile', profile)
 backendApp.use('/api/users', users)
 backendApp.use('/api/playlists', playlists)
 
-const port = process.env.PORT || 5000
-
-backendApp.listen(port, () => console.log(`\nServer running on port ${port}`))
+backendApp.listen(process.env.PORT || 5000, () => console.log(`\nServer running on port ${port}`))
